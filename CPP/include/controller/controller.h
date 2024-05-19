7f19637924d68f067365c3f803399c4736d00ff1
Swathi0712 <swathimangala82@gmail.com>
2024-05-19 21:45:23 +0530

    Refact: implemented traffic log, switch addition and removal in the controller

--- CPP/include/controller/controller.h
+++ CPP/include/controller/controller.h
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include "../switch/switch.h"
 
 using namespace std;
 
 class Controller{
+    private:
+        vector <Switch> switches;
     public:
         // method to send control message to switches
         void sendControlMessage(string message){
@@ -13,14 +18,20 @@ class Controller{
         void handleNetworkEvent(string event){
             cout << "Handling network event:" << event << endl;
         }
-
+        // Add a new switch to the controller
+        void addSwitch(const Switch& swt){
+            switches.push_back(swt);
+            cout << "Switch added to the conroller" << endl;
+        }
+        // Remove switch from the controller
+        void removeSwitch(const Switch& swt){
+            auto item = find(switches.begin(), switches.end(),swt);
+            if(item != switches.end()){
+                switches.erase(item);
+                cout << "Switch removed from the controller" << endl;
+            }
+            else{
+                cout << "Switch not found" << endl;
+            }
+        }
 };
-
-// int main()
-// {
-//     // creating an instance
-//     Controller controller; 
-//     // send a control message
-//     controller.sendControlMessage("message");
-
-// }
--- CPP/include/switch/switch.h
+++ CPP/include/switch/switch.h
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Switch{
     private:
         string id;
         bool status;
+        vector <string> trafficLogs;
     public:
         // Initialising Switch status and switch ID
         Switch(string switchID, bool switchStatus){
@@ -15,7 +17,7 @@ class Switch{
         // Function to set the status of the switch
         void setStatus(bool newStatus){
             status = newStatus;
-            cout << "New status:" << (status?"on":"off") <<endl;
+            cout << "New status:" << (status?"on":"off") << endl;
         }
         // Function to get the status of the switch
         bool getStatus(){
@@ -30,10 +32,15 @@ class Switch{
         }
         void logTraffic(string traffic){
             // Push to traffic logs
-
+            trafficLogs.push_back(traffic);
+            cout << "Switch" << id << "Traffic logged" << traffic << endl;
         }
         void showTrafficLogs(){
             // To print out the traffic logs from the log table
+            cout << "Traffic log for switch" << id << endl;
+            for(auto log : trafficLogs){
+                cout << log << endl;
+            }
         }
 
 };
