a4f5cbb142542fe3bf0e869fb986beb2c912b137
Swathi0712 <swathimangala82@gmail.com>
2024-05-24 21:34:36 +0530

    Create: Switch class and its member functions using python

---
+++ py/src/switch/switch.py
@@ -0,0 +1,41 @@
+class Switch:
+    # private members
+    __id = None
+    __status = None
+    __trafficLogs = []
+
+    # public members
+    # initialising switch Id and status
+    def __init__(self, switchId , switchStatus, TrafficLogs):
+        self.__id = switchId
+        self.__status = switchStatus
+        self.__trafficLogs = TrafficLogs
+
+
+    # Function to set the status of the switch
+    def setStatus(self, newStatus):
+        self.__status = newStatus
+        print("New status", "on" if self.__status == True else "Off")
+    
+    # Function to get the status of the switch
+    def getStatus(self):
+        return self.__status
+    
+    def getId(self):
+        return self.__id
+    
+    # Function to display received control message
+    def receiveControlMessage(self, message):
+        print("Switch: ", self.__id , "Received control message", message)
+
+    # Push to traffic logs
+    def logTraffic(self, traffic):
+        self.__trafficLogs.append(traffic)
+        print("Switch: ", self.__id, "Traffic loggeg ", traffic)
+    
+    # To print out the traffic logs from the log table
+    def showTrafficLogs(self):
+        print("Traffic log for switch: ", self.__ids)
+        for log in self.__trafficLogs:
+            print(log)
+    
