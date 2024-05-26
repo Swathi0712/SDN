680ffd58b2846b5c04a1bd8db67db4ec0d2ab77d
Swathi0712 <swathimangala82@gmail.com>
2024-05-26 23:39:38 +0530

    Update: update the routing table based on received routing updates from the switches in python

--- py/src/controller/controller.py
+++ py/src/controller/controller.py
@@ -1,19 +1,33 @@
 import sys
 sys.path.append(r'D:\SDN\py\src\switch')
 from switch import Switch
+import math
+
+class RoutingTableEntry:
+    # private members
+    destination = ""
+    nextHop = []       
+    cost = math.inf
+    
+    def __init__(self, dest, nxtH, c):
+        self.destination = dest
+        self.nextHop = nxtH
+        self.cost = c
 
 # Create controller class
 class Controller:
     # private members
     # List to store switch objects
     __switches = []
-
+    __routingTable = [] 
+    
     # public members
+
     # Add a new switch to the controller
     def addSwitch(self, swt):
         self.__switches.append(swt)
         print("Switch added to controller")
-    
+
     # Remove switch from the controller
     def removeSwitch(self, swt):
         found = False
@@ -29,7 +43,39 @@ class Controller:
     # method to send control message to all switches
     def sendControlMessage(self, message):
         for i in self.__switches:
-            i.receivedControlMessage(message)
+            i.receiveControlMessage(message)
         print(" Control message sent to all switches ")
     
     # Method to handle network events
+    def handleNetworkEvent(self, event):
+        print("Handled network event:")
+        # Perform actions based on network events
+        if event == "Link Up":
+            # Send a message to all switches to enable ports(Set status as Active)
+            self.sendControlMessage("EnablePorts")
+        elif event == "Link Down":
+            # Send a message to all switches to disable ports(Set status as Inactive)
+            self.sendControlMessage("DisablePorts")
+            
+    # Method to Update the routing table based on received routing updates from the switches
+    def updateRoutingTable(self, updates):
+        for entry in updates:
+            it = None
+            # Check if entry is already available on the routing table
+            for idx, route in enumerate(self.__routingTable):
+                if route.destination == entry.destination:
+                    it = idx
+                    break
+           
+            if(it!=None):
+                if(entry.cost < self.__routingTable[it].cost):
+                    self.__routingTable[it] = entry
+                elif(entry.cost == self.__routingTable[it].cost):
+                    self.__routingTable[it].nextHop.append(entry.nextHop[0])
+            
+            else:
+                self.__routingTable.append(entry)
+                
+    # Method to perform routing based on destination IP Address
+    
+                
--- py/src/main.py
+++ py/src/main.py
@@ -1,8 +1,12 @@
 import sys
-sys.path.append(r'D:\SDN\py\src\switch')
+sys.path.insert(0, 'D:\\SDN\\py\\src\\switch')
 from switch import Switch
-sys.path.append(r'D:\SDN\py\src\controller')
-from controller import Controller
+sys.path.insert(0,'D:\\SDN\\py\\src\\controller')
+# import controller
+# from typing import TYPE_CHECKING 
+# if TYPE_CHECKING:
+from controller import Controller, RoutingTableEntry as rte
+
 
 # Create a controller instance
 control = Controller()
@@ -17,6 +21,29 @@ control.addSwitch(switch2)
 
 # Send control message to all switches
 control.sendControlMessage("Activate")
+
+# Log traffic
+switch1.logTraffic("Packet1")
+switch1.logTraffic("Packet2")
+switch2.logTraffic("Packet3")
+
+# Show traffic logs
+switch1.showTrafficLogs()
+switch2.showTrafficLogs()
+
+# Simulate network events
+e1 = rte("destination_A", ["switch2"], 2)
+e2 = rte("destination_B", ["switch2"], 2)
+update1 = [e1, e2]
+
+e3 = rte("destination_A", ["switch1"], 2)
+e4 = rte("destination_B", ["switch1"], 2)
+update2 = [e3, e4]
+
+control.updateRoutingTable(update1)
+control.updateRoutingTable(update2)
+
+
 
 
 
